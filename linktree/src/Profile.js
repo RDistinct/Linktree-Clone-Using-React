b73baf3ba04a8e2dd41bc40749d799c2d84198c1
ReubenDev <reubenkiarie8m@gmail.com>
2022-10-29 18:41:18 +0300

    linktree clone

---
+++ linktree/src/Profile.js
@@ -0,0 +1,11 @@
+const profile = () => {
+    return ( 
+        <div className="profileContainer">
+            <img id="profile__img"src="" alt="" srcset="" />
+            <h3 {/*id="slack"*/}>RuBen</h3>
+            
+        </div>
+     );
+}
+ 
+export default profile;
